1f41271df3e6551a14d8a81d7bb38caac62fd8b0
huron <aarougaldet@gmail.com>
2025-06-30 18:34:17 -0600

    last comiit

--- depression/models_tests.py
+++ depression/models_tests.py
@@ -3,6 +3,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+import joblib
+
 
 data_classification = pd.read_csv('resources/preprocessed_student_depression_data.csv')
 data_classification = data_classification.drop(columns=['Family History of Mental Illness'])
@@ -10,37 +13,36 @@ data_classification = data_classification.drop(columns=['Family History of Menta
 X_class = data_classification.drop(columns=['Depression'])
 y_class = data_classification['Depression']
 X_train_class, X_test_class, y_train_class, y_test_class = train_test_split(X_class, y_class, test_size=0.2, random_state=42)
-model_class = RandomForestClassifier(n_estimators=300, random_state=42, max_depth=10, min_samples_split=5)
-model_class.fit(X_train_class, y_train_class)
-y_pred_class = model_class.predict(X_test_class)
-print(f'Classification Report:\n{model_class.score(X_test_class, y_test_class)}')
-print(f'Confusion Matrix:\n{pd.crosstab(y_test_class, y_pred_class, rownames=["Actual"], colnames=["Predicted"])}')
-print(f'Accuracy: {model_class.score(X_test_class, y_test_class)}')
-print(f'F1 Score: {model_class.score(X_test_class, y_test_class)}')
-print(f'Precision: {model_class.score(X_test_class, y_test_class)}')
-print(f'Recall: {model_class.score(X_test_class, y_test_class)}')
-print(f'ROC AUC: {model_class.score(X_test_class, y_test_class)}')
+random_forest_model = RandomForestClassifier(n_estimators=300, random_state=42, max_depth=10, min_samples_split=5)
+random_forest_model.fit(X_train_class, y_train_class)
+y_pred_class = random_forest_model.predict(X_test_class)
+print(f'RF: Classification Report:\n{random_forest_model.score(X_test_class, y_test_class)}')
+print(f'RF: Confusion Matrix:\n{pd.crosstab(y_test_class, y_pred_class, rownames=["Actual"], colnames=["Predicted"])}')
+print(f'RF: Accuracy: {accuracy_score(y_test_class, y_pred_class)}')
+print(f'RF: F1 Score: {f1_score(y_test_class, y_pred_class)}')
+print(f'RF: Precision: {precision_score(y_test_class, y_pred_class)}')
+print(f'RF: Recall: {recall_score(y_test_class, y_pred_class)}')
 
 model_tree = DecisionTreeClassifier(random_state=42, max_depth=10, min_samples_split=5)
 model_tree.fit(X_train_class, y_train_class)
 y_pred_tree = model_tree.predict(X_test_class)
 print(f'Decision Tree Classification Report:\n{model_tree.score(X_test_class, y_test_class)}')
 print(f'Decision Tree Confusion Matrix:\n{pd.crosstab(y_test_class, y_pred_tree, rownames=["Actual"], colnames=["Predicted"])}')
-print(f'Decision Tree Accuracy: {model_tree.score(X_test_class, y_test_class)}')
-print(f'Decision Tree F1 Score: {model_tree.score(X_test_class, y_test_class)}')
-print(f'Decision Tree Precision: {model_tree.score(X_test_class, y_test_class)}')
-print(f'Decision Tree Recall: {model_tree.score(X_test_class, y_test_class)}')
-print(f'Decision Tree ROC AUC: {model_tree.score(X_test_class, y_test_class)}')
+print(f'Decision Tree Accuracy: {accuracy_score(y_test_class, y_pred_tree)}')
+print(f'Decision Tree F1 Score: {f1_score(y_test_class, y_pred_tree)}')
+print(f'Decision Tree Precision: {precision_score(y_test_class, y_pred_tree)}')
+print(f'Decision Tree Recall: {recall_score(y_test_class, y_pred_tree)}')
 
 knn_model = KNeighborsClassifier(n_neighbors=5)
 knn_model.fit(X_train_class, y_train_class)
 y_pred_knn = knn_model.predict(X_test_class)
 print(f'KNN Classification Report:\n{knn_model.score(X_test_class, y_test_class)}')
 print(f'KNN Confusion Matrix:\n{pd.crosstab(y_test_class, y_pred_knn, rownames=["Actual"], colnames=["Predicted"])}')
-print(f'KNN Accuracy: {knn_model.score(X_test_class, y_test_class)}')
-print(f'KNN F1 Score: {knn_model.score(X_test_class, y_test_class)}')
-print(f'KNN Precision: {knn_model.score(X_test_class, y_test_class)}')
-print(f'KNN Recall: {knn_model.score(X_test_class, y_test_class)}')
-print(f'KNN ROC AUC: {knn_model.score(X_test_class, y_test_class)}')
+print(f'KNN Accuracy: {accuracy_score(y_test_class, y_pred_knn)}')
+print(f'KNN F1 Score: {f1_score(y_test_class, y_pred_knn)}')
+print(f'KNN Precision: {precision_score(y_test_class, y_pred_knn)}')
+print(f'KNN Recall: {recall_score(y_test_class, y_pred_knn)}')
+
+joblib.dump(random_forest_model, 'resources/rf_model.pkl')
 
 
--- depression/preproces.py
+++ depression/preproces.py
@@ -4,7 +4,7 @@ import numpy as np
 
 data = pd.read_csv('resources/Student Depression Dataset.csv')
 
-preprocesed_data = data.drop(columns=['id', 'City', 'Profession'])
+preprocesed_data = data.drop(columns=['id', 'City', 'Profession', 'Work Pressure'])
 
 values = preprocesed_data['Degree'].unique()
 print(values)
@@ -26,24 +26,27 @@ def map_degree_level(degree):
         return -1  # Otros/no clasificados
 
 
-gender_encoder = LabelEncoder()
-preprocesed_data.loc[:, ['Gender']] = gender_encoder.fit_transform(preprocesed_data['Gender'])
+# Encode Gender
+preprocesed_data.loc[:, ['Gender']] = preprocesed_data['Gender'].apply(lambda x: 1 if x == 'Female' else 0)
 
+# Encode Sleep Duration
 order_sleep = ['Less than 5 hours', '5-6 hours', 'Others', '7-8 hours', 'More than 8 hours']
 sleep_duration_encoder = OrdinalEncoder(categories=[order_sleep])
 preprocesed_data.loc[:, ['Sleep Duration']] = sleep_duration_encoder.fit_transform(preprocesed_data[['Sleep Duration']])
 
+# Encode Dietary Habits
 preprocesed_data.loc[:, ['Dietary Habits']] = preprocesed_data[['Dietary Habits']].replace('Others', 'Moderate')
 order_dietary = ['Unhealthy', 'Moderate', 'Healthy']
 dietary_habits_encoder = OrdinalEncoder(categories=[order_dietary])
 preprocesed_data.loc[:, ['Dietary Habits']] = dietary_habits_encoder.fit_transform(preprocesed_data[['Dietary Habits']])
 
-suicidal_thoughts_encoder = LabelEncoder()
-preprocesed_data.loc[:, ['Have you ever had suicidal thoughts ?']] = suicidal_thoughts_encoder.fit_transform(preprocesed_data['Have you ever had suicidal thoughts ?']) 
+# Encode Have you ever had suicidal thoughts?
+preprocesed_data.loc[:, ['Have you ever had suicidal thoughts ?']] = preprocesed_data['Have you ever had suicidal thoughts ?'].apply(lambda x: 1 if x == 'Yes' else 0)
 
-family_history_encoder = LabelEncoder()
-preprocesed_data.loc[:, ['Family History of Mental Illness']] = family_history_encoder.fit_transform(preprocesed_data['Family History of Mental Illness'])
+# Encode Family History of Mental Illness
+preprocesed_data.loc[:, ['Family History of Mental Illness']] = preprocesed_data['Family History of Mental Illness'].apply(lambda x: 1 if x == 'Yes' else 0)
 
+## Encode Degree
 preprocesed_data.loc[:, ['Degree']] = preprocesed_data['Degree'].apply(map_degree_level)
 
 def fillna_random(df):
@@ -55,9 +58,13 @@ def fillna_random(df):
             )
     return df
 
+# 1) Rellenas todo el df
 preprocesed_data = fillna_random(preprocesed_data)
 
 preprocesed_data.to_csv('resources/preprocessed_student_depression_data.csv', index=False)
 
+
+for col in preprocesed_data.columns:
+    print(f"{col}: {preprocesed_data[col].min()}, {preprocesed_data[col].max()}")
 
 
